d165dd8493dc97b26c04a20f525f02628e860b25
Dracnea <dracneagenie@gmail.com>
2021-01-31 18:01:32 -0500

    Add Spell Color Enum

--- ClassifedSpells.js
+++ ClassifedSpells.js
@@ -1,14 +1,37 @@
 // ==UserScript==
 // @name         Classfied Spells
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      3.0
 // @description  Add color and extra text to backpack.tf listings with spells
 // @author       Dracnea
 // @match        https://backpack.tf/classifieds*
 // @grant        none
 // ==/UserScript==
 
+const Spells  = {
+    //voices
+    VOICES: "bfbfbf",
+    //colors
+    PUTRESCENT: "ccff33",
+    SINISTER: "66ff66",
+    DIEJOB: "cccc00",
+    SPECTRAL: "ff9900",
+    CHROMATIC: "aa80ff",
+    //footprints
+    VIOLET: "ffa366",
+    CORPSE: "9fdfbf",
+    GANGREEN: "ffff00",
+    BRUISED: "ff6666",
+    TEAMSPIRIT: "ff471a",
+    ROTTEN: "ff9933",
+    HEADLESS: "300099",
+    //error/new spells
+    DEFAULT: "f442bc"
+
+}
+
 (function() {
     'use strict';
-    
+
 })();
+
