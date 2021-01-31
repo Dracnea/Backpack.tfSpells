3e017942666d327a76db5ae08479a0c8c9777e32
Dracnea <dracneagenie@gmail.com>
2021-01-31 17:50:16 -0500

    Add Tampermonkey Comments and Settings

---
+++ ClassifedSpells.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Classfied Spells
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  Add color and extra text to backpack.tf listings with spells
+// @author       Dracnea
+// @match        https://backpack.tf/classifieds*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+})();
