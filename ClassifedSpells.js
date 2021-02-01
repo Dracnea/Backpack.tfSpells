bd6e4790636ca41772fcdf4ed078525b0b5340dd
Dracnea <dracneagenie@gmail.com>
2021-01-31 20:47:00 -0500

    Add main and spellSearch functions

--- ClassifedSpells.js
+++ ClassifedSpells.js
@@ -8,7 +8,7 @@
 // @grant        none
 // ==/UserScript==
 
-const SpellColors  = {
+var SpellColors = {
     //voices (and doubles for exorcism)
     VOICES: "bfbfbf",
     //colors
@@ -27,62 +27,127 @@ const SpellColors  = {
     //this will double for pumpkin bombs
     ROTTEN: "ff9933",
     HEADLESS: "300099",
+    //weapons
+    EXORCISM: "bfbfbf",
+    FIRE: "66ff66",
+    BOMBS: "ff9933",
     //error/new spells
     DEFAULT: "f442bc"
-
 }
 
-const SpellNames = {
+var SpellNames = {
     //voices
     VOICES: "Voices From Below",
     //colors
-    PUTRESCENT: "",
-    SINISTER: "",
-    DIEJOB: "",
-    SPECTRAL: "",
-    CHROMATIC: "",
+    PUTRESCENT: "Putrescent Pigmentation",
+    SINISTER: "Sinster Staining",
+    DIEJOB: "Die Job",
+    SPECTRAL: "Spectral Spectrum",
+    CHROMATIC: "Chromatic Corruption",
     //footprints
-    VIOLET: "",
-    CORPSE: "",
-    GANGREEN: "",
-    BRUISED: "",
-    TEAMSPIRIT: "",
-    ROTTEN: "",
-    HEADLESS: "",
+    VIOLET: "Violent Violent",
+    CORPSE: "Corpse Gray",
+    GANGREEN: "Gangreen",
+    BRUISED: "Bruised Purple",
+    TEAMSPIRIT: "Team Sprit",
+    ROTTEN: "Rotten Orange",
+    HEADLESS: "Headless Horseshoes",
     //weapons
-    EXORCISM: "",
-    FIRE: "",
-    BOMBS: "",
+    EXORCISM: "Exorcism",
+    FIRE: "Halloween Fire",
+    BOMBS: "Pumpkin Bombs",
     //error/new spells
-    DEFAULT: "f442bc"
+    DEFAULT: "Default"
 }
 
 (function() {
     'use strict';
+    var newRows = document.querySelectorAll("div.item")
+    var newDesc = document.querySelectorAll("h5")
 
+    for(var i = 0; i < newRows.length; i++){
+        var spell1 = "data-spell_1"
+        var spell2 = "data-spell_2"
+
+        if(newRows[i].hasAttributes(spell1)) {
+            spellSearch(spell1, newRows[i]);
+            newDesc[i].textContent += "\r\n" + newRows[i].getAttribute(spell1) + " ";
+
+            if(newRows[i].hasAttributes(spell2)){
+                spellSearch(spell2, newRows[i])
+                newDesc[i].textContent += "\r\n" + newRows[i].getAttribute(spell2);
+            }
+        }
+    }
 })();
 
 function spellSearch(spellNum, thisRow) {
     var thisRowParent = thisRow.parentNode;
     var thisRowGrandParent = thisRow.parentNode.parentNode;
-    
+
     var spellName = thisRow.getAttribute(spellNum);
 
     var currentStyle = thisRowGrandParent.getAttribute("style")
 
+    var currentColor = determineSpell(spellName)
 
-
+    switch(currentColor) {
+        case SpellColors.FIRE:
+        case SpellColors.BOMBS:
+        case SpellColors.VIOLET:
+        case SpellColors.CORPSE:
+        case SpellColors.GANGREEN:
+        case SpellColors.BRUISED:
+        case SpellColors.TEAMSPIRIT:
+        case SpellColors.ROTTEN:
+        case SpellColors.HEADLESS:
+            thisRowParent.setAttributes("style", currentStyle + ";background-color: " + currentColor)
+            break
+        default:
+            thisRowGrandParent.setAttributes("style", currentStyle + ";background-color: " + currentColor)
+            break
+    }
 
 }
 
-function determineSpell(spellName) {    PUTRESCENT: "ccff33",
+function determineSpell(spellName) {
 //thiswill double for halloween fire
     var start = "Halloween Spell: "
+    var footprints = " Footprints"
     switch(spellName) {
         case start + SpellNames.VOICES:
             return SpellColors.VOICES
+        case start + SpellNames.PUTRESCENT:
+            return SpellColors.PUTRESCENT
+        case start + SpellNames.SINISTER:
+            return SpellColors.SINISTER
+        case start + SpellNames.DIEJOB:
+            return SpellColors.DIEJOB
+        case start + SpellNames.SPECTRAL:
+            return SpellColors.SPECTRAL
+        case start + SpellNames.CHROMATIC:
+            return SpellColors.CHROMATIC
+        case start + SpellNames.VIOLET + footprints:
+            return SpellColors.VIOLET
+        case start + SpellNames.CORPSE + footprints:
+            return SpellColors.CORPSE
+        case start + SpellNames.GANGREEN + footprints:
+            return SpellColors.GANGREEN
+        case start + SpellNames.BRUISED + footprints:
+            return SpellColors.BRUISED
+        case start + SpellNames.TEAMSPIRIT + footprints:
+            return SpellColors.TEAMSPIRIT
+        case start + SpellNames.ROTTEN + footprints:
+            return SpellNames.ROTTEN
+        case start + SpellNames.HEADLESS:
+            return SpellColors.HEADLESS
+        case start + SpellNames.EXORCISM:
+            return SpellColors.EXORCISM
+        case start + SpellNames.FIRE:
+            return SpellColors.FIRE
+        case start + SpellNames.BOMBS:
+            return SpellColors.BOMBS
         default:
-            return 
+            return SpellColors.DEFAULT
     }
 }
-
